84f5781321049408a44c97c557379c5018fc10ad
Micolta J <Micolta J@Micolta>
2016-09-03 00:58:54 -0500

    Revert "commit"
    This reverts commit b597b19bb9aded07b90413654231dede93279e28.

--- travis/src/main.java
+++ travis/src/main.java
@@ -16,7 +16,6 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
-        System.out.println("hello word");
     }
     
 }
